f6fce2618887075e6c549720e41156992a73eaa7
Miria <soda.mia@gmail.com>
2018-02-13 10:55:16 -0800

    adding stickerjs the heroku link

--- src/comp/stickers.js
+++ src/comp/stickers.js
@@ -16,7 +16,7 @@ class Stickers extends Component {
     }
     
     componentDidMount(){
-        this.socket = mySocket("http://localhost:10000");
+        this.socket = mySocket("https://stickersockets.herokuapp.com");
         
         this.socket.on("userjoined", (data)=>{
            this.setState({
