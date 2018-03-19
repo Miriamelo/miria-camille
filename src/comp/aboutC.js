f932b7653186e3bf757aac009eb6866e6eb2eab2
camillecamahort <camillecamahort@gmail.com>
2018-03-19 15:32:00 -0700

    updated camille overview

--- src/comp/aboutC.js
+++ src/comp/aboutC.js
@@ -22,6 +22,13 @@ class AboutC extends Component {
       <div className="App">
         <img src={Header} className="camProfile" />
         <img src={Arrow} className="arrow" onClick={this.goHome} />
+        <p>
+        I am an aspiring UX/UI and interaction designer. I am currently enrolled in BCIT’s Digital Design and Development program, where I have developed a strong foundation in creating interactive and dynamic applications and websites. <br /> <br />
+
+        I am fuelled by my curiosity, which is what sparked my interest in UX. I am always eager to learn new things and my curiosity is what drives me to find solutions to everyday problems.<br /> <br />
+
+        I have taken on numerous side projects such as designing and creating websites and posters for startups based in the Philippines. These projects have helped my with my skills in design, HTML, CSS, and WordPress.
+        </p>
           
       </div>
     );
